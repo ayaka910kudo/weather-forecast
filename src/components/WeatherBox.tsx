99b2c1ed82aebd5f009c8b30c15612747d7e7b61
ayaka910kudo <aya.caro.brbn8@gmail.com>
2024-10-15 23:24:42 +0900

    mapで展開した際のデータについて型定義を作成

--- src/components/WeatherBox.tsx
+++ src/components/WeatherBox.tsx
@@ -5,6 +5,7 @@ import { useWeather } from "../hooks/useWeather";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import HourlyWeatherItem from "./HourlyWeatherItem";
+import { ThreeHoursWeatherDataList } from "@/types/types";
 
 const WeatherBox = () => {
   const { weatherData, threeHoursWeatherData } = useWeather("Hiratsuka");
@@ -122,7 +123,7 @@ const WeatherBox = () => {
             }}
           >
             {threeHoursWeatherData?.list &&
-              threeHoursWeatherData.list.slice(1, 5).map((item, index: number) => (
+              threeHoursWeatherData.list.slice(1, 5).map((item: ThreeHoursWeatherDataList, index: number) => (
                 <HourlyWeatherItem
                   key={index}
                   hour={new Date(item.dt * 1000).getHours()} // UNIXタイムスタンプを時間に変換
--- src/types/types.ts
+++ src/types/types.ts
@@ -102,3 +102,41 @@ export type ThreeHoursWeatherApiResponse = {
     sunset: number;
   };
 };
+
+export type ThreeHoursWeatherDataList = {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  rain?: {
+    "3h": number;
+  };
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+};
